1fbb9d9c5b9f00b2bffe0ef9ae402eecdb3feecf
¨miguelamador23¨ <miguelamador23@outlook.com>
2024-01-15 13:36:11 -0600

    Version 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import UserCards from './components/UserCards'
+
+
+function App() {
+
+
+  return (
+    <>
+      <UserCards />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UserCards.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import './UserCards.css';
+import SearchBar from './SearchBar';
+
+function UserCards() {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const response = await fetch('src/users.json');
+        const data = await response.json();
+        setUsers(data);
+        setFilteredUsers(data);
+      } catch (error) {
+        console.error('Error al obtener los usuarios:', error);
+      }
+    };
+
+    getUsers();
+  }, []);
+
+  const handleSearch = (searchTerm) => {
+    const filteredData = users.filter((user) =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.role.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredUsers(filteredData);
+  };
+
+  return (
+    <>
+      <SearchBar onSearch={handleSearch} />
+      <div className="card-container">
+        {filteredUsers.map((user) => (
+          <div key={user.id} className="card">
+            {user.avatar ? (
+              <img src={user.avatar} alt="" className="card-img" />
+            ) : (
+              <div className="placeholder-img"></div>
+            )}
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <p>{user.role}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default UserCards;
